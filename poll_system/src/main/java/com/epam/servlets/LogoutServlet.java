b0c147ccc6fca0267710265cadfa9082b3fe7929
Gohar Minasyan <goharminasyan80@gmail.com>
2022-03-15 16:19:03 +0400

    poll app

--- poll_system/src/main/java/com/epam/servlets/LogoutServlet.java
+++ poll_system/src/main/java/com/epam/servlets/LogoutServlet.java
@@ -1,15 +1,8 @@
 package com.epam.servlets;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.io.PrintWriter;
 
-import static java.lang.System.out;
+import java.io.IOException;
+
 
 @WebServlet(urlPatterns ="/logout")
 public class LogoutServlet extends HttpServlet {
